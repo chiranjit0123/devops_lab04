30f0992b48a2d35338b1e48f48e02d78e38c07b3
root <chiranjitgorai0123@gmail.com>
2025-07-14 15:20:17 +0530

    update

---
+++ demo/src/test/java/bits/_wa86835/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bits._wa86835.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
